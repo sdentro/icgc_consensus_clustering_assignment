8d0a14528e3e7d933d80b0f8eb2591ba4b4d7d0f
sdentro <sd11@sanger.ac.uk>
2017-03-16 12:03:21 +0000

    First complete script for snv and indel ready to test

--- run.R
+++ run.R
@@ -1,13 +1,14 @@
 args = commandArgs(T)
 
 samplename = args[1]
-vcf_file = args[2]
-bb_file = args[3]
-clust_file = args[4]
-purity_file = args[5]
-summary_table = args[6]
-mult_file = args[7]
-dpc_assign_file = args[8]
+snv_vcf_file = args[2]
+indel_vcf_file = args[3]
+bb_file = args[4]
+clust_file = args[5]
+purity_file = args[6]
+summary_table = args[7]
+mult_file = args[8]
+dpc_assign_file = args[9]
 merge_clusters = F
 
 
@@ -17,29 +18,41 @@ merge_clusters = F
 #purity_file = "dp/20161213_vanloo_wedge_consSNV_prelimConsCNAallStar/1_purity_ploidy/purity_ploidy.txt"
 
 source("~/repo/moritz_mut_assignment/MutationTime.R")
+source("~/repo/moritz_mut_assignment/util.R")
 source("~/repo/dpclust3p/R/interconvertMutationBurdens.R")
 library(ggplot2)
 library(gridExtra)
 library(grid)
 
-vcf <- readVcf(vcf_file, genome="GRCh37")
+########################################################################
+# Parse the input
+########################################################################
+vcf_snv <- readVcf(snv_vcf_file, genome="GRCh37")
+vcf_indel <- readVcf(indel_vcf_file, genome="GRCh37")
+
+# TODO SV
+# data_sv = parse_sv_data("test_data/1e27cc8a-5394-4958-9af6-5ece1fe24516_most_likely_copynumbers.txt", "test_data/1e27cc8a-5394-4958-9af6-5ece1fe24516_vaf_ccf.txt")
+
 bb <- loadBB(bb_file)
 clusters = read.table(clust_file, header=TRUE, sep="\t")
+
 purityPloidy <- read.table(purity_file, header=TRUE, sep="\t")
 purity = purityPloidy$purity[purityPloidy$samplename==samplename]
 ploidy = purityPloidy$ploidy[purityPloidy$samplename==samplename]
+
 summary_table = read.table(summary_table, header=T, stringsAsFactors=F)
-mult = read.table(mult_file, header=T, stringsAsFactors=F)
-dpc_assign = read.table(dpc_assign_file, header=T, stringsAsFactors=F)
+# Commented out for now
+# mult = read.table(mult_file, header=T, stringsAsFactors=F)
+# dpc_assign = read.table(dpc_assign_file, header=T, stringsAsFactors=F)
 sex = summary_table$inferred_sex[summary_table$samplename==samplename]
 is_wgd = purityPloidy$wgd_status[purityPloidy$samplename==samplename]=="wgd"
 
-#' If not all clusters are tehre we need to renumber them
+#' If not all clusters are there we need to renumber them
 if (max(clusters$cluster) > nrow(clusters)) {
-	dpc_assign_input = dpc_assign
+	# dpc_assign_input = dpc_assign
 	for (i in 1:nrow(clusters)) {
 		clusterid = clusters$cluster[i]
-		dpc_assign$cluster[dpc_assign_input$cluster==clusterid] = i
+		# dpc_assign$cluster[dpc_assign_input$cluster==clusterid] = i
 		clusters$cluster[i] = i
 	}
 }	
@@ -48,33 +61,86 @@ if (max(clusters$cluster) > nrow(clusters)) {
 #' Convert copy number clonal frequency to CCF
 bb$clonal_frequency = bb$clonal_frequency / purity
 
-if (merge_clusters) {
-	  clusters = mergeClusters(clusters)
-}
+#' Merge clusters if requested
+if (merge_clusters) { clusters = mergeClusters(clusters) }
+
+#' Calculate CCF for each cluster
 clusters$ccf = clusters$proportion/purity
 
-MCN <- computeMutCn(vcf, bb, clusters, purity, gender=sex, isWgd=is_wgd)
+########################################################################
+# Assignments
+########################################################################
+#' Assign using Moritz' approach
+MCN <- computeMutCn(vcf_snv, bb, clusters, purity, gender=sex, isWgd=is_wgd)
+MCN_indel <- computeMutCn(vcf_indel, bb, clusters, purity, gender=sex, isWgd=is_wgd)
 
-#' Calculate bionomial probability of each mutation belonging in each cluster
-getClustLL = function(data, cluster_locations, purity) {
-	assignment_ll = sapply(1:length(cluster_locations), function(c) {
-			        mutBurdens = mutationCopyNumberToMutationBurden(cluster_locations[c] * data$MutCN, data$MajCN+data$MinCN, purity, rep(2, nrow(data)))
-				data$altCount*log(mutBurdens) + data$wtCount*log(1-mutBurdens)
-				})
-	return(assignment_ll)
-}
+#' TODO: add SV
 
-clust_assign_ll = getClustLL(MCN$D, clusters$proportion/purity, purity)
-best_cluster = unlist(apply(clust_assign_ll, 1, function(x) if (all(is.na(x))) NA else which.max(x)))
+########################################################################
+# Create the assignment - binom probability
+########################################################################
+snv_binom = assign_binom_ll(MCN, clusters, purity)
+# plot_data = res$plot_data
+# clusters_new = res$clusters_new
 
-cluster_counts = table(best_cluster)
-clusters_new = data.frame(clusters$cluster, sapply(clusters$cluster, function(x) cluster_counts[[as.character(x)]]), clusters$proportion, clusters$ccf)
-colnames(clusters_new) = colnames(clusters)
+indel_binom = assign_binom_ll(MCN_indel, clusters, purity)
 
-#' Calculate MCN and CCF for plotting
-mcn = mutationBurdenToMutationCopyNumber(burden=MCN$D$altCount / (MCN$D$altCount + MCN$D$wtCount), cellularity=purity, normalCopyNumber=rep(2, nrow(MCN$D)), totalCopyNumber=MCN$D$MajCN + MCN$D$MinCN)
-ccf = mcn / MCN$D$MutCN
+#' TODO: add SV
 
+########################################################################
+# Create the assignment - Kaixians approach
+########################################################################
+snv_moritz = assign_moritz(MCN, clusters, purity)
+# plot_data_2 = res$plot_data
+# clusters_new_2 = res$clusters_new
+
+indel_moritz = assign_moritz(MCN_indel, clusters, purity)
+
+#' TODO: add SV
+
+########################################################################
+# Summary table entry
+########################################################################
+sample_entry = get_summary_table_entry(samplename=samplename, 
+                                       summary_table=summary_table, 
+                                       clusters=snv_moritz$clusters_new, 
+                                       assignments_table=snv_moritz$plot_data, 
+                                       indel_assignments=indel_moritz$plot_data, 
+                                       sv_assignments=NULL)
+write.table(sample_entry, file=paste0(samplename, "_summary_table_entry.txt"), quote=F, sep="\t", row.names=F)
+
+########################################################################
+# Output to share with PCAWG
+########################################################################
+snv_timing = data.frame(chromosome=as.character(seqnames(vcf_snv)),
+                        position=as.numeric(start(vcf_snv)),
+                        mut_type=rep("SNV", nrow(MCD$D)),
+                        timing=classifyMutations(MCN$D))
+
+snv_output = data.frame(chromosome=as.character(seqnames(vcf_snv)),
+                        position=as.numeric(start(vcf_snv)),
+                        ccf=snv_moritz$clusters$ccf[match(snv_moritz$plot_data$cluster, snv_moritz$clusters$cluster)])
+
+indel_timing = data.frame(chromosome=as.character(seqnames(vcf_indel)),
+                          position=as.numeric(start(vcf_indel)),
+                          mut_type=rep("indel", nrow(MCN_indel$D)),
+                          timing=classifyMutations(MCN_indel$D))
+
+indel_output = data.frame(chromosome=as.character(seqnames(vcf_indel)),
+                          position=as.numeric(start(vcf_indel)),
+                          ccf=clusters_new_2$ccf[match(indel_moritz$plot_data$cluster, indel_moritz$clusters$cluster)])
+
+timing = rbind(snv_timing, indel_timing)
+ccfs = rbind(snv_output, indel_output)
+
+readr::write_tsv(timing, paste0(samplename, "_timing_snv_indel.txt"))
+readr::write_tsv(ccfs, paste0(samplename, "_ccfs_snv_indel.txt"))
+
+#' TODO: add SV
+
+########################################################################
+# Plot
+########################################################################
 base_plot = function(plot_data, x_variable, title=NA) {
   p = ggplot(plot_data) + aes_string(x=x_variable, y="..count..", fill="cluster") + geom_histogram(binwidth=0.02, colour="black", position="stack") + ylab("Count") + theme(legend.position="bottom") + scale_fill_discrete(drop = FALSE)
   if (!is.na(title)) {
@@ -83,18 +149,21 @@ base_plot = function(plot_data, x_variable, title=NA) {
   return(p)
 }
 
-plot_data = data.frame(mcn=mcn, ccf=ccf, cluster=factor(best_cluster, levels=rev(unique(sort(clusters_new$cluster)))))
-p = base_plot(plot_data, "ccf", "Consensus binomial assignment") + xlim(0, 1.5) + geom_vline(data=clusters, mapping=aes(xintercept=ccf))
-p2 = base_plot(plot_data, "mcn", "Consensus binomial assignment") + xlim(0, 4)
-
-p = p + scale_fill_hue(labels = rev(paste0(" ", clusters_new$cluster, " : ", round(clusters_new$ccf, 2), "  ", clusters_new$n_ssms, "  ")))
-
-g_legend<-function(a.gplot){ 
+g_legend <- function(a.gplot) { 
   tmp <- ggplot_gtable(ggplot_build(a.gplot)) 
   leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box") 
   legend <- tmp$grobs[[leg]] 
-  return(legend)} 
+  return(legend)
+} 
 
+#' Make assignment plot for both assignment strategies
+p = base_plot(snv_binom$plot_data, "ccf", "Consensus binomial assignment") + xlim(0, 1.5) + geom_vline(data=clusters, mapping=aes(xintercept=ccf))
+# p2 = base_plot(plot_data, "mcn", "Consensus binomial assignment") + xlim(0, 4)
+p3 = base_plot(snv_moritz$plot_data, "ccf", "Consensus closest cluster assignment") + xlim(0, 1.5) + geom_vline(data=clusters, mapping=aes(xintercept=ccf))
+p = p + scale_fill_hue(labels = rev(paste0(" ", 
+                                           snv_binom$clusters$cluster, " : ", 
+                                           round(snv_binom$clusters$ccf, 2), "  ", 
+                                           snv_binom$clusters$n_ssms, "  ")))
 my_legend = g_legend(p)
 
 if (samplename %in% summary_table$samplename) {
@@ -106,55 +175,39 @@ if (samplename %in% summary_table$samplename) {
 }
 title = paste0(samplename, " - ",
                histology, " - ",
-               "SNVs ", nrow(plot_data), " - ",
+               "SNVs ", nrow(snv_binom$plot_data), " - ",
                "Purity ", purity, " - ",
                "Ploidy ", ploidy, " - ",
                "Power ", power)
 
-#' Kaixians approach
-best_cluster = sapply(MCN$D$CNF, function(x) if (is.na(x)) NA else which.min(abs(x-clusters_new$proportion)))
-cluster_counts = table(best_cluster)
-clusters_new_2 = data.frame(clusters$cluster, sapply(clusters$cluster, function(x) cluster_counts[[as.character(x)]]), clusters$proportion, clusters$ccf)
-colnames(clusters_new_2) = colnames(clusters)
-
-plot_data_2 = data.frame(mcn=mcn, ccf=MCN$D$CNF, cluster=factor(best_cluster, levels=rev(unique(sort(clusters_new$cluster)))))
-plot_data_2$ccf = plot_data_2$ccf / purity
-p3 = base_plot(plot_data_2, "ccf", "Consensus closest cluster assignment") + xlim(0, 1.5) + geom_vline(data=clusters, mapping=aes(xintercept=ccf))
-
-#' DPClust output - sync the data frames
-assign_chr_pos = paste0(dpc_assign$chr, "_", dpc_assign$pos)
-mult_chr_pos = paste0(mult$chr, "_", mult$pos)
-inboth = intersect(mult_chr_pos, assign_chr_pos)
-dpc_assign = dpc_assign[assign_chr_pos %in% inboth,]
-mult = mult[mult_chr_pos %in% inboth,]
-
-
-ccf = mult$mutation.copy.number / mult$multiplicity
-plot_data_4 = data.frame(mcn=mult$mutation.copy.number, ccf=ccf, cluster=factor(dpc_assign$cluster, levels=rev(unique(sort(clusters$cluster)))))
-p4 = base_plot(plot_data_4, "ccf", "DPClust assignment") + xlim(0, 1.5) + geom_vline(data=clusters, mapping=aes(xintercept=ccf))
 
 png(paste0(samplename, "_final_assignment.png"), height=400, width=1000)
 grid.arrange(arrangeGrob(p + theme(legend.position="none"), 
-			 p3 + theme(legend.position="none"), ncol=2), 
-	     arrangeGrob(my_legend), nrow=2, heights=c(9,1), top=title)
-dev.off()
-
-png(paste0(samplename, "_final_assignment_all.png"), height=800, width=1000)
-grid.arrange(arrangeGrob(p + theme(legend.position="none"), 
-			 p3 + theme(legend.position="none"),
-			 p4 + theme(legend.position="none"),
-			 p2 + theme(legend.position="none"), ncol=2), 
-	     arrangeGrob(my_legend), nrow=2, heights=c(18,1), top=title)
+                         p3 + theme(legend.position="none"), ncol=2), 
+             arrangeGrob(my_legend), nrow=2, heights=c(9,1), top=title)
 dev.off()
 
 save.image(paste0(samplename, "_assignment.RData"))
-#' # Check output
-#' # Mutation Annotation
-#' head(MCN$D)
-# Classify as basic clonal states
-# table(classifyMutations(MCN$D))
-# Timing parameters
-# MCN$P[[1]]
-# Extract timing of segments
-# bb$timing_param <- MCN$P
-# bbToTime(bb)
+
+# No longer used
+# #' DPClust output - sync the data frames
+# assign_chr_pos = paste0(dpc_assign$chr, "_", dpc_assign$pos)
+# mult_chr_pos = paste0(mult$chr, "_", mult$pos)
+# inboth = intersect(mult_chr_pos, assign_chr_pos)
+# dpc_assign = dpc_assign[assign_chr_pos %in% inboth,]
+# mult = mult[mult_chr_pos %in% inboth,]
+# 
+# 
+# ccf = mult$mutation.copy.number / mult$multiplicity
+# plot_data_4 = data.frame(mcn=mult$mutation.copy.number, ccf=ccf, cluster=factor(dpc_assign$cluster, levels=rev(unique(sort(clusters$cluster)))))
+# p4 = base_plot(plot_data_4, "ccf", "DPClust assignment") + xlim(0, 1.5) + geom_vline(data=clusters, mapping=aes(xintercept=ccf))
+# 
+# 
+# png(paste0(samplename, "_final_assignment_all.png"), height=800, width=1000)
+# grid.arrange(arrangeGrob(p + theme(legend.position="none"), 
+# 			 p3 + theme(legend.position="none"),
+# 			 p4 + theme(legend.position="none"),
+# 			 p2 + theme(legend.position="none"), ncol=2), 
+# 	     arrangeGrob(my_legend), nrow=2, heights=c(18,1), top=title)
+# dev.off()
+
---
+++ util.R
@@ -0,0 +1,220 @@
+########################################################################
+# Parsers
+########################################################################
+
+#' Obtain for every SV altCount, tumDepth and majorCN
+#' 
+#' @param cn_assignment_file An SVclone SV to copy number assignment file
+#' @param vafs_file An SVclone file reporting VAFs per SV
+#' @return A data.frame with altCount, tumDepth and majorCN columns
+#' 
+#' @author sd11
+#' @export
+parse_sv_data = function(cn_assignment_file, vafs_file) {
+  cn_assignment = read.table(cn_assignment_file, header=T, stringsAsFactors=F, sep="\t")
+  vafs = read.table(vafs_file, header=T, stringsAsFactors=F, sep="\t")
+  
+  parse_entry = function(cn_assignment, vafs, i) {
+    vaf = vafs$adjusted_VAF[i]
+    pos1_cn = cn_assignment$pos1_bb_CN[i]
+    pos2_cn = cn_assignment$pos2_bb_CN[i]
+    total_cn = as.numeric(unlist(stringr::str_split(ifelse(pos1_cn!="", pos1_cn, pos2_cn), ","))[1])
+    depth = cn_assignment$depth[i]
+    # sv_data = rbind(sv_data, data.frame(altCount=vaf*depth, tumDepth=depth, majorCN=total_cn))
+    return(data.frame(altCount=vaf*depth, tumDepth=depth, majorCN=total_cn))
+  }
+  
+  res = lapply(1:nrow(vafs), parse_entry, cn_assignment=cn_assignment, vafs=vafs)
+  return(as.data.frame(do.call(rbind, res)))
+}
+
+
+#' Create a custom temp SV vcf file that goes into computeMutCn. Using the dummy vcf means
+#' the computeMutCn does not need to be modified for the different use case that are SVs.
+create_dummy_sv_vcf = function(svclone_output) {
+  
+  #' Get preferred breakpoints -> thtat are mapped to cna <- add chr/pos of selected to parse_sv_data output
+  #' 
+  #' Format into vcf <- load a template
+  #' 
+  #' return(vcf)
+  
+}
+
+########################################################################
+# Mutation Assignment
+########################################################################
+#' Calculate binomial probability of each mutation belonging in each cluster
+#' 
+#' @param data
+#' @param cluster_locations
+#' @param purity The sample purity
+#' 
+getClustLL = function(data, cluster_locations, purity) {
+  assignment_ll = sapply(1:length(cluster_locations), function(c) {
+    mutBurdens = mutationCopyNumberToMutationBurden(cluster_locations[c] * data$MutCN, data$MajCN+data$MinCN, purity, rep(2, nrow(data)))
+    data$altCount*log(mutBurdens) + data$wtCount*log(1-mutBurdens)
+  })
+  return(assignment_ll)
+}
+
+#' Assign mutations to the cluster with highest binomial likelihood
+#' 
+#' @param MCN Output from computeMutCn
+#' @param clusters data.frame with cluster number, size, proportion and ccf
+#' @param purity The sample purity
+#' 
+#' @return A list containing two data.frames: (1) The mutations with mcn, ccf and assigned cluster and (2) Cluster number, size, proportion and ccf
+#' @author sd11
+assign_binom_ll = function(MCN, clusters, purity) {
+  clust_assign_ll = getClustLL(MCN$D, clusters$proportion/purity, purity)
+  best_cluster = unlist(apply(clust_assign_ll, 1, function(x) if (all(is.na(x))) NA else which.max(x)))
+  
+  cluster_counts = table(best_cluster)
+  clusters_new = data.frame(clusters$cluster, sapply(clusters$cluster, function(x) cluster_counts[[as.character(x)]]), clusters$proportion, clusters$ccf)
+  colnames(clusters_new) = colnames(clusters)
+  
+  #' Calculate MCN and CCF for plotting
+  mcn = mutationBurdenToMutationCopyNumber(burden=MCN$D$altCount / (MCN$D$altCount + MCN$D$wtCount), cellularity=purity, normalCopyNumber=rep(2, nrow(MCN$D)), totalCopyNumber=MCN$D$MajCN + MCN$D$MinCN)
+  ccf = mcn / MCN$D$MutCN
+  plot_data = data.frame(mcn=mcn, ccf=ccf, cluster=factor(best_cluster, levels=rev(unique(sort(clusters_new$cluster)))))
+  return(list(plot_data=plot_data, clusters_new=clusters_new))
+}
+
+#' Assign mutations to the best cluster identified by Moritz method
+#' 
+#' @param MCN Output from computeMutCn
+#' @param clusters data.frame with cluster number, size, proportion and ccf
+#' @param purity The sample purity
+#' 
+#' @return A list containing two data.frames: (1) The mutations with mcn, ccf and assigned cluster and (2) Cluster number, size, proportion and ccf
+#' @author sd11
+assign_moritz = function(MCN, clusters, purity) {
+  best_cluster = sapply(MCN$D$CNF, function(x) if (is.na(x)) NA else which.min(abs(x-clusters_new$proportion)))
+  cluster_counts = table(best_cluster)
+  clusters_new_2 = data.frame(clusters$cluster, sapply(clusters$cluster, function(x) cluster_counts[[as.character(x)]]), clusters$proportion, clusters$ccf)
+  colnames(clusters_new_2) = colnames(clusters)
+  
+  plot_data_2 = data.frame(mcn=mcn, ccf=MCN$D$CNF, cluster=factor(best_cluster, levels=rev(unique(sort(clusters_new$cluster)))))
+  plot_data_2$ccf = plot_data_2$ccf / purity
+  return(list(plot_data=plot_data_2, clusters_new=clusters_new_2))
+}
+
+########################################################################
+# PCAWG11 Summary table
+########################################################################
+
+get_clusters_entry = function(clusters, assignments_table, indel_assignments=NULL, sv_assignments=NULL, min_clonal_ccf=0.9, max_clonal_ccf=1.1) {
+  assignments = table(assignments_table$cluster)
+  total_muts = sum(assignments)
+  if (total_muts < 100) {
+    threshold = total_muts*FRAC_SNVS_CLUSTER
+  } else {
+    threshold = 30
+  }
+  
+  # kept_clusters = names(assignments)[assignments > (total_muts*FRAC_SNVS_CLUSTER) & assignments > MIN_NUM_SNVS_CLUSTER]
+  kept_clusters = names(assignments)[assignments > threshold]
+  superclones_to_merge = names(assignments)[clusters$location > max_clonal_ccf & clusters$no.of.mutations < total_muts*FRAC_SNVS_CLUSTER & rep(length(kept_clusters) > 1, length(kept_clusters))]
+  
+  # kept_clusters = names(assignments)
+  
+  # Count the number of subclones and SNVs assigned
+  num_clonal = 0
+  num_subclonal = 0
+  num_subclones = 0
+  num_superclones = 0
+  num_superclonal = 0
+  indel_clonal = 0
+  indel_subclonal = 0
+  indel_superclonal = 0
+  sample_entry$sv_clonal = 0
+  sample_entry$sv_subclonal = 0
+  sample_entry$sv_superclonal = 0
+  cluster_locations = c()
+  cluster_sizes = c()
+  for (cluster in rev(kept_clusters)) {
+    if (clusters[clusters$cluster.no==cluster,]$location > min_clonal_ccf) {
+      # Clonal
+      num_clonal = num_clonal + assignments[cluster]
+      if (!is.null(indel_assignments)) { indel_clonal = indel_clonal + indel_assignments[cluster] }
+      if (!is.null(sv_assignments)) { sv_clonal = sv_clonal + sv_assignments[cluster] }
+      
+      if (clusters[clusters$cluster.no==cluster,]$location > max_clonal_ccf & ! cluster %in% superclones_to_merge) {
+        # Superclonal
+        num_superclones = num_superclones + 1
+        num_superclonal = num_superclonal + assignments[cluster]
+        if (!is.null(indel_assignments)) { indel_superclonal = indel_superclonal + indel_assignments[cluster] }
+      }
+    } else {
+      # Subclonal
+      num_subclonal = num_subclonal + assignments[cluster]
+      num_subclones = num_subclones + 1
+      if (!is.null(indel_assignments)) { indel_subclonal = indel_subclonal + indel_assignments[cluster] }
+      if (!is.null(sv_assignments)) { sv_subclonal = sv_subclonal + sv_assignments[cluster] }
+    }
+    
+    #' TODO: indel and sv here required ?
+    if (! cluster %in% superclones_to_merge) {
+      cluster_locations = c(cluster_locations, clusters[clusters$cluster.no==cluster,]$location)
+      
+      temp_cluster_size = assignments[cluster]
+      if (!is.null(indel_assignments)) { temp_cluster_size = temp_cluster_size + indel_assignments[cluster] }
+      if (!is.null(sv_assignments)) { temp_cluster_size = temp_cluster_size + sv_assignments[cluster] }
+      cluster_sizes = c(cluster_sizes, temp_cluster_size)
+    }
+  }
+  return(list(clust_stats=list(num_subclones, 
+                               num_clonal, num_subclonal, num_superclones, num_superclonal, 
+                               indel_clonal, indel_subclonal, indel_superclonal, 
+                               sv_clonal, sv_subclonal, sv_superclonal), 
+              clust_details=list(cluster_locations, cluster_sizes)))
+}
+
+
+FRAC_SNVS_CLUSTER = 0.01
+
+#' Creates a summary table entry
+#' 
+#' @param samplename The samplename
+#' @param summary_table An existing summary table with an entry for this sample
+#' @param clusters_new_2
+#' @param plot_data_2
+#' 
+#' @return 
+#' @author sd11
+get_summary_table_entry = function(samplename, summary_table, cluster_info, snv_assignment_table, indel_assignment_table=NULL, sv_assignment_table=NULL) {
+  sample_entry = summary_table[summary_table$samplename==samplename, ,drop=F]
+  # temp because using old summary table
+  sample_entry$indel_clonal = 0
+  sample_entry$indel_subclonal = 0
+  sample_entry$indel_superclonal = 0
+  sample_entry$sv_clonal = 0
+  sample_entry$sv_subclonal = 0
+  sample_entry$sv_superclonal = 0
+  colnames(cluster_info) = c("cluster.no", "no.of.muts", "proportion", "location")
+  
+  # Get the values for various SNV cluster related columns
+  res = get_clusters_entry(cluster_info, 
+                           snv_assignment_table, 
+                           indel_assignments=indel_assignment_table, 
+                           sv_assignments=sv_assignment_table)
+  clust_stats = lapply(list(res), function(x) x$clust_stats)
+  
+  # Saving these for later in the script to be appended to the table
+  clust_details = lapply(list(res), function(x) x$clust_details)
+  
+  # Combine into single row matrix and then copy the updated values into the sample_entry
+  res = as.data.frame(matrix(unlist(clust_stats), ncol=11, byrow=T))
+  colnames(res) = c("num_subclones", "num_clonal", "num_subclonal", "num_superclones", "num_superclonal", "indel_clonal", "indel_subclonal", "indel_superclonal")
+  res$frac_clonal = round(res$num_clonal / (res$num_subclonal+res$num_clonal), 3)
+  sample_entry[, colnames(sample_entry) %in% colnames(res)] = res
+  
+  # Append cluster details
+  res = lapply(clust_details, function(x) data.frame(paste(round(x[[1]], 4), collapse=";"), paste(x[[2]], collapse=";")))
+  res = do.call(rbind, res)
+  colnames(res) = c("cluster_locations", "cluster_sizes")
+  sample_entry[, colnames(sample_entry) %in% colnames(res)] = res
+  return(sample_entry)
+}
+
