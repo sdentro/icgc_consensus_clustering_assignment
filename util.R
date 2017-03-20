3d935ec760e91e60c6c652be41969e57c10a3855
sdentro <sd11@sanger.ac.uk>
2017-03-20 21:26:27 +0000

    updating probs code to work with varying nr of clusters

--- util.R
+++ util.R
@@ -312,40 +312,35 @@ pcawg11_output = function(snv_moritz, indel_moritz, sv_moritz, MCN, MCN_indel, M
     sv_mult = NULL
   }
   
-  # Obtain probabilities - SNV
-  n_subclones = nrow(final_clusters)-1
-  r = t(t(sapply(MCN$D$pAllSubclones, function(x) if(length(x)!=0) x else rep(NA, n_subclones))))
-  snv_assignments_prob = data.frame(chr=as.character(seqnames(vcf_snv)), 
-                                    pos=as.numeric(start(vcf_snv)), 
-                                    clone=1-rowSums(r), 
-                                    r)
-  # set cluster number in the header
-  colnames(snv_assignments_prob) = c("chr", "pos", paste0("cluster_", final_clusters$cluster))
+  get_probs = function(final_clusters, MCN, vcf_snv) {
+    n_subclones = nrow(final_clusters)-1
+    if (n_subclones==1) {
+      r = t(t(sapply(MCN$D$pAllSubclones, function(x) if(length(x)!=0) x else rep(NA, n_subclones))))
+    } else {
+      r = sapply(MCN$D$pAllSubclones, function(x) if(length(x)!=0) x else rep(NA, n_subclones))
+    }
+    snv_assignments_prob = data.frame(chr=as.character(seqnames(vcf_snv)), 
+                                      pos=as.numeric(start(vcf_snv)), 
+                                      clone=1-rowSums(r), 
+                                      r)
+    # set cluster number in the header
+    colnames(snv_assignments_prob) = c("chr", "pos", paste0("cluster_", final_clusters$cluster))
+    return(snv_assignments_prob)
+  }
+  
+  # Obtain probabilities - snv
+  snv_assignments_prob = get_probs(final_clusters, MCN, vcf_snv)
   
   if (nrow(indel_assignments) > 0) {
     # Obtain probabilities - indel
-    n_subclones = nrow(final_clusters)-1
-    r = t(t(sapply(MCN_indel$D$pAllSubclones, function(x) if(length(x)!=0) x else rep(NA, n_subclones))))
-    indel_assignments_prob = data.frame(chr=indel_assignments$chr, 
-                                        pos=indel_assignments$pos,
-                                        clone=1-rowSums(r), 
-                                        r)
-    # set cluster number in the header
-    colnames(indel_assignments_prob) = paste0("cluster_", final_clusters$cluster)
+    indel_assignments_prob = get_probs(final_clusters, MCN_indel, vcf_indel)
   } else {
     indel_assignments_prob = NULL
   }
   
   if (!is.null(vcf_sv)) {
     # Obtain probabilities - SV
-    n_subclones = nrow(final_clusters)-1
-    r = t(t(sapply(MCN_sv$D$pAllSubclones, function(x) if(length(x)!=0) x else rep(NA, n_subclones))))
-    sv_assignments_prob = data.frame(chr=sv_assignments$chr, 
-                                     pos=sv_assignments$pos, 
-                                     clone=1-rowSums(r), 
-                                     r)
-    # set cluster number in the header
-    colnames(sv_assignments_prob) = paste0("cluster_", final_clusters$cluster)
+    sv_assignments_prob = get_probs(final_clusters, MCN_sv, vcf_sv)
   } else {
     sv_assignments_prob
   }
