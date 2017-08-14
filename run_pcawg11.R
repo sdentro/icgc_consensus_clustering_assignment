b1b0786b8c9408db144225b9ea3d49b805a1c3c7
Stefan Dentro <sdentro@gmail.com>
2017-08-14 12:24:36 +0100

    Adding multiplicity and some notes

--- run_pcawg11.R
+++ run_pcawg11.R
@@ -1,3 +1,6 @@
+# Dump output in PCAWG11 format (i.e. the pcawg11_output.RData file from run.R)
+# later runs do not need the samplename parsing as the variable is already included in the archive
+
 args = commandArgs(T)
 infile = args[1]
 outdir = args[2]
@@ -6,3 +9,4 @@ load(infile)
 samplename = paste(unlist(stringr::str_split(basename(infile), "_"))[1:3], collapse="_")
 write.table(final_pcawg11_output$final_clusters, file=paste0(outdir, samplename, "_subclonal_structure.txt"), row.names=F, sep="\t", quote=F)
 write.table(final_pcawg11_output$snv_assignments, file=paste0(outdir, samplename,"_mutation_assignments.txt"), row.names=F, sep="\t", quote=F)
+write.table(final_pcawg11_output$snv_mult, file=paste0(outdir, samplename,"_multiplicity.txt"), row.names=F, sep="\t", quote=F)
